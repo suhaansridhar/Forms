fb9d12d424948defa3e97abc849d9ec01cea0ffc
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-18 20:09:51 +0100

    added sample19

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { createContext, useState } from 'react';
-import Form18Sample01 from './Form18Sample01'
-
-export const Theme = createContext();
+import React from 'react'
+import Form19Sample from './Form19Sample'
 
 const App = () => {
-  const [darkTheme, setDarkTheme] = useState(true);
-
-  function toggle(){
-    setDarkTheme(prevState => !prevState);
-  }
-
-  const buttonStyle = {
-    backgroundColor : darkTheme ? 'black' : 'blue',
-    color: darkTheme ? 'white' : 'black'
-  }
-
   return (
-    <Theme.Provider value={darkTheme}>
-      <Form18Sample01 />
-      <button onClick={toggle} style={buttonStyle}>clickme</button>
-    </Theme.Provider>
+    <div>
+      <Form19Sample />
+    </div>
   )
 }
 
